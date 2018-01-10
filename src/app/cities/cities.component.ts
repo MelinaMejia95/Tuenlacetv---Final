f390460b6bb607415864a503ef4a051b73c8f408
Melina Mejia Bedoya <melinamejia95@gmail.com>
2018-01-09 21:39:10 -0500

    Card created in Cities component

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let jQuery: any;
+
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.component.html',
@@ -10,6 +12,7 @@ export class CitiesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    jQuery('select').material_select();
   }
 
 }
