a6ba4261f3fd59503abb6a2604902269f9658e1c
Melina Mejia Bedoya <melinamejia95@gmail.com>
2018-02-18 20:42:59 -0500

    Banks component configured

--- src/app/banks/banks.component.ts
+++ src/app/banks/banks.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let jQuery:any;
+
 @Component({
   selector: 'app-banks',
   templateUrl: './banks.component.html',
@@ -7,9 +9,104 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BanksComponent implements OnInit {
 
+  banks: any[] = ['Bancolombia', 'Bancopopular', 'Bogotá'];
+  toogleDelete:boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    jQuery('select').material_select();
+    jQuery('#modal-crear').modal();
+    jQuery('#modal-see').modal({ complete: function() { 
+      jQuery('#codigoEdit').prop('disabled',false);
+      jQuery('#nitEdit').prop('disabled',false);
+      jQuery('#direccionEdit').prop('disabled',false);
+      jQuery('#tel1Edit').prop('disabled',false);
+      jQuery('#tel2Edit').prop('disabled',false);
+      jQuery('#ciudadEdit').prop('disabled',false);
+      jQuery('#contactoEdit').prop('disabled',false);
+      jQuery('#cuenta_banEdit').prop('disabled',false);
+      jQuery('#cuenta_conEdit').prop('disabled',false);
+      jQuery('#selectEdit').prop('disabled',false);
+     }});
+  }
+
+  openModal () {
+    jQuery('#modal-see').modal('open');
+    document.getElementsByClassName('table-radio');
+    //jQuery('.table-radio').attr('checked');
+  }
+
+  closeModal () {
+    jQuery('#modal-see').modal('close');
+    
+  }
+
+  selectAll() {
+    var check = <HTMLInputElement><any>document.getElementsByName('group1');
+    var radios = <HTMLInputElement><any>document.getElementsByName('group2');
+    var cantidad = document.getElementsByName('group1');
+    var rows = <HTMLInputElement><any>document.getElementsByName('rows');
+    
+    if (radios[0].checked){
+      document.getElementById('btn-footer-delete').setAttribute('style', 'visibility: visible');
+      console.log(cantidad.length)
+      for(var i = 0; i < cantidad.length; i++ ) {
+        check[i].checked = true;
+        rows[i].setAttribute("style", "background-color : #9ad1ea");
+      }
+    } else {
+      document.getElementById('btn-footer-delete').setAttribute('style', 'visibility: hidden');
+      for(var i = 0; i < cantidad.length; i++ ) {
+        check[i].checked = false;
+        rows[i].setAttribute("style", "background-color : none");
+      }
+    }
+  }
+
+  selectRow() {
+    var rows = <HTMLInputElement><any>document.getElementsByName('rows');
+    var radios = <HTMLInputElement><any>document.getElementsByName('group2');
+    var check = <HTMLInputElement><any>document.getElementsByName('group1');
+    var cantidad = document.getElementsByName('group1');
+
+    if (this.toogleDelete == true) {
+      document.getElementById('btn-footer-delete').setAttribute('style', 'visibility: hidden');
+      this.toogleDelete = false;
+    }
+    
+    for(var i = 0; i < cantidad.length; i++ ){
+      if(check[i].checked){
+        console.log('false');
+        this.toogleDelete = true;
+        document.getElementById('btn-footer-delete').setAttribute('style', 'visibility: visible');
+        rows[i].setAttribute("style", "background-color : #9ad1ea");
+      } else {
+        rows[i].setAttribute("style", "background-color : none");
+      }
+    }    
+  }
+
+  edit () {
+    jQuery('#codigoEdit').prop('disabled',false);
+    jQuery('#nitEdit').prop('disabled',false);
+    jQuery('#direccionEdit').prop('disabled',false);
+    jQuery('#tel1Edit').prop('disabled',false);
+    jQuery('#tel2Edit').prop('disabled',false);
+    jQuery('#ciudadEdit').prop('disabled',false);
+    jQuery('#contactoEdit').prop('disabled',false);
+    jQuery('#cuenta_banEdit').prop('disabled',false);
+    jQuery('#cuenta_conEdit').prop('disabled',false);
+    jQuery('#selectEdit').prop('disabled',false);
+    jQuery('#codigoEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#nitEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#direccionEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#tel1Edit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#tel2Edit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#ciudadEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#contactoEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#cuenta_banEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#cuenta_conEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
   }
 
 }
