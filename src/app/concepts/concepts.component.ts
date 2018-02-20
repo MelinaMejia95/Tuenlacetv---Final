b5c8fbcc0e860496441827775ecef1ea5fe93dee
Melina Mejia Bedoya <melinamejia95@gmail.com>
2018-02-19 20:42:19 -0500

    Concepts component configured

--- src/app/concepts/concepts.component.ts
+++ src/app/concepts/concepts.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let jQuery:any;
+
 @Component({
   selector: 'app-concepts',
   templateUrl: './concepts.component.html',
@@ -7,9 +9,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConceptsComponent implements OnInit {
 
+  toogleDelete:boolean = false;
+  concepts: any[] = ['1', '2', '3'];
+
   constructor() { }
 
   ngOnInit() {
+    jQuery('select').material_select();
+    jQuery('#modal-crear').modal();
+    jQuery('#modal-see').modal({ complete: function() { 
+      jQuery('#codigoEdit').prop('disabled',true);
+      jQuery('#tiposervicioEdit').prop('disabled',true);
+      jQuery('#nombreEdit').prop('disabled',true);
+      jQuery('#ivaEdit').prop('disabled',true);
+      jQuery('#operacionEdit').prop('disabled',true);
+     }});
+  }
+
+  openModal () {
+    jQuery('#modal-see').modal('open');
+    document.getElementsByClassName('table-radio');
+  }
+
+  closeModal () {
+    jQuery('#modal-see').modal('close');
+    
+  }
+
+  selectAll() {
+    var check = <HTMLInputElement><any>document.getElementsByName('group1');
+    var radios = <HTMLInputElement><any>document.getElementsByName('group2');
+    var cantidad = document.getElementsByName('group1');
+    var rows = <HTMLInputElement><any>document.getElementsByName('rows');
+    
+    if (radios[0].checked){
+      document.getElementById('btn-footer-delete').setAttribute('style', 'visibility: visible');
+      console.log(cantidad.length)
+      for(var i = 0; i < cantidad.length; i++ ) {
+        check[i].checked = true;
+        rows[i].setAttribute("style", "background-color : #9ad1ea");
+      }
+    } else {
+      document.getElementById('btn-footer-delete').setAttribute('style', 'visibility: hidden');
+      for(var i = 0; i < cantidad.length; i++ ) {
+        check[i].checked = false;
+        rows[i].setAttribute("style", "background-color : none");
+      }
+    }
+  }
+
+  selectRow() {
+    var rows = <HTMLInputElement><any>document.getElementsByName('rows');
+    var radios = <HTMLInputElement><any>document.getElementsByName('group2');
+    var check = <HTMLInputElement><any>document.getElementsByName('group1');
+    var cantidad = document.getElementsByName('group1');
+
+    if (this.toogleDelete == true) {
+      document.getElementById('btn-footer-delete').setAttribute('style', 'visibility: hidden');
+      this.toogleDelete = false;
+    }
+    
+    for(var i = 0; i < cantidad.length; i++ ){
+      if(check[i].checked){
+        console.log('false');
+        this.toogleDelete = true;
+        document.getElementById('btn-footer-delete').setAttribute('style', 'visibility: visible');
+        rows[i].setAttribute("style", "background-color : #9ad1ea");
+      } else {
+        rows[i].setAttribute("style", "background-color : none");
+      }
+    }    
+  }
+
+  edit () {
+    jQuery('#codigoEdit').prop('disabled',false);
+    jQuery('#tiposervicioEdit').prop('disabled',false);
+    jQuery('#nombreEdit').prop('disabled',false);
+    jQuery('#ivaEdit').prop('disabled',false);
+    jQuery('#operacionEdit').prop('disabled',false);
+    jQuery('#codigoEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#tiposervicioEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#nombreEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#ivaEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#operacionEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
   }
 
 }
