3d334021ed0c73d798fb70a7f6092a87695b0a09
Melina Mejia Bedoya <melinamejia95@gmail.com>
2018-02-19 20:17:27 -0500

    Rates component configured, except datepicker

--- src/app/rates/rates.component.ts
+++ src/app/rates/rates.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let jQuery:any;
+
 @Component({
   selector: 'app-rates',
   templateUrl: './rates.component.html',
@@ -7,9 +9,111 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatesComponent implements OnInit {
 
+  toogleDelete:boolean = false;
+  rates: any[] = ['1', '2', '3'];
+
   constructor() { }
 
   ngOnInit() {
+    jQuery('select').material_select();
+    jQuery('#modal-crear').modal();
+    jQuery('#modal-see').modal({ complete: function() { 
+      jQuery('#codigoEdit').prop('disabled',true);
+      jQuery('#zonaEdit').prop('disabled',true);
+      jQuery('#conceptoEdit').prop('disabled',true);
+      jQuery('#planEdit').prop('disabled',true);
+      jQuery('#valorEdit').prop('disabled',true);
+      jQuery('#estadoEdit').prop('disabled',true);
+      jQuery('#fechainicioEdit').prop('disabled',true);
+      jQuery('#fechafinEdit').prop('disabled',true);
+     }});
+     jQuery('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      today: 'Hoy',
+      clear: 'Limpiar',
+      close: 'Ok',
+      closeOnSelect: false, // Close upon selecting a date,
+      monthsFull: [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ],
+      weekdaysFull: [ 'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado' ],
+      weekdaysShort: [ 'Dom', 'Lun', 'Mar', 'Mier', 'Jue', 'Vier', 'Sáb' ],
+      format: 'dd/mm/yyyy'
+    });
   }
+
+  openModal () {
+    jQuery('#modal-see').modal('open');
+    document.getElementsByClassName('table-radio');
+  }
+
+  closeModal () {
+    jQuery('#modal-see').modal('close');
+    
+  }
+
+  selectAll() {
+    var check = <HTMLInputElement><any>document.getElementsByName('group1');
+    var radios = <HTMLInputElement><any>document.getElementsByName('group2');
+    var cantidad = document.getElementsByName('group1');
+    var rows = <HTMLInputElement><any>document.getElementsByName('rows');
+    
+    if (radios[0].checked){
+      document.getElementById('btn-footer-delete').setAttribute('style', 'visibility: visible');
+      console.log(cantidad.length)
+      for(var i = 0; i < cantidad.length; i++ ) {
+        check[i].checked = true;
+        rows[i].setAttribute("style", "background-color : #9ad1ea");
+      }
+    } else {
+      document.getElementById('btn-footer-delete').setAttribute('style', 'visibility: hidden');
+      for(var i = 0; i < cantidad.length; i++ ) {
+        check[i].checked = false;
+        rows[i].setAttribute("style", "background-color : none");
+      }
+    }
+  }
+
+  selectRow() {
+    var rows = <HTMLInputElement><any>document.getElementsByName('rows');
+    var radios = <HTMLInputElement><any>document.getElementsByName('group2');
+    var check = <HTMLInputElement><any>document.getElementsByName('group1');
+    var cantidad = document.getElementsByName('group1');
+
+    if (this.toogleDelete == true) {
+      document.getElementById('btn-footer-delete').setAttribute('style', 'visibility: hidden');
+      this.toogleDelete = false;
+    }
+    
+    for(var i = 0; i < cantidad.length; i++ ){
+      if(check[i].checked){
+        console.log('false');
+        this.toogleDelete = true;
+        document.getElementById('btn-footer-delete').setAttribute('style', 'visibility: visible');
+        rows[i].setAttribute("style", "background-color : #9ad1ea");
+      } else {
+        rows[i].setAttribute("style", "background-color : none");
+      }
+    }    
+  }
+
+  edit () {
+    jQuery('#codigoEdit').prop('disabled',false);
+    jQuery('#zonaEdit').prop('disabled',false);
+    jQuery('#conceptoEdit').prop('disabled',false);
+    jQuery('#planEdit').prop('disabled',false);
+    jQuery('#valorEdit').prop('disabled',false);
+    jQuery('#estadoEdit').prop('disabled',false);
+    jQuery('#fechainicioEdit').prop('disabled',false);
+    jQuery('#fechafinEdit').prop('disabled',false);
+    jQuery('#codigoEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#zonaEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#conceptoEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#planEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#valorEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#estadoEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#fechainicioEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#fechafinEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+  }
+
 
 }
