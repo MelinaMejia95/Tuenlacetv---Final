5e795bee70d5f0d944043705aa8c6b382d61dd6e
Melina Mejia Bedoya <melinamejia95@gmail.com>
2018-03-02 13:40:47 -0500

    Neighborhoods CRUD ready

--- src/app/neighborhoods/neighborhoods.component.ts
+++ src/app/neighborhoods/neighborhoods.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NeighborhoodsService } from '../services/neighborhoods.service';
+import swal from 'sweetalert2';
 
 declare let jQuery:any;
 
@@ -9,12 +11,22 @@ declare let jQuery:any;
 })
 export class NeighborhoodsComponent implements OnInit {
 
-  neighborhoods: any[] = ['Belen', 'Rosales', 'Aliadas'];
-  toogleDelete:boolean = false;
+  neighborhoods: any[] = [];
+  toogleDelete: boolean = false;
+  zones: string;
+  neighborhoodEdit: any;
+  zoneEdit: any;
+  neighborhood: any;
+  createZone: string;
 
-  constructor() { }
+  constructor(private _neighborhoodservice: NeighborhoodsService) { }
 
   ngOnInit() {
+    this._neighborhoodservice.getNeighborhoods().subscribe(data => {
+      this.neighborhoods = data.barrios;
+      this.zones = data.zonas;
+      console.log(this.zones[0]['nombre'])
+    });
     jQuery('select').material_select();
     jQuery('#modal-crear').modal();
     jQuery('#modal-see').modal({ complete: function() { 
@@ -22,9 +34,114 @@ export class NeighborhoodsComponent implements OnInit {
         jQuery('#nombreEdit').prop('disabled',true);
         jQuery('#selectEdit').prop('disabled',true);
        }});
+    jQuery('#select-zone').on('change', () => {
+      this.createZone = jQuery('#select-zone').val();
+      console.log(this.createZone)
+    });
   }
 
-  openModal () {
+  selectData(neighborhood){
+    this.neighborhoodEdit = neighborhood;
+  }
+
+  createNeighborhood(name){
+    if (name) {
+      this._neighborhoodservice.createNeighborhoodss({ 'zona_id': this.createZone, 'nombre': name, 'db': localStorage.getItem('db'), 'usuario_id': localStorage.getItem('usuario_id') }).subscribe(
+        data => {
+          if ( data.status == "created") {
+            swal({
+              title: 'Registro creado con éxito',
+              text: '',
+              type: 'success',
+              onClose: function reload() {
+                        location.reload();
+                      }
+            })
+          } else {
+            swal(
+              'No se pudo crear el registro',
+              '',
+              'warning'
+            )
+          }
+        });
+    }
+  } 
+
+  updateNeighborhood() {
+    if(this.neighborhoodEdit){
+      this._neighborhoodservice.updateNeighborhoods({'zona_id': this.neighborhood, 'nombre': this.neighborhoodEdit.nombre, 'id': this.neighborhoodEdit.id, 'usuario_id': localStorage.getItem('usuario_id'),
+                                    'db': localStorage.getItem('db')}).subscribe(
+        data => {
+          console.log(data)
+          if ( data.status == "updated") {
+            swal({
+              title: 'Registro actualizado con éxito',
+              text: '',
+              type: 'success',
+              onClose: function reload() {
+                        location.reload();
+                      }
+            })
+          } else {
+            swal(
+              'No se pudo eactualizar el registro',
+              '',
+              'warning'
+            )
+          }
+        }
+      );
+    }
+  }
+
+  deleteNeighborhood() {
+    swal({
+      title: '¿Desea eliminar el registro?',
+      text: "",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+        if (this.neighborhoodEdit) {
+          this._neighborhoodservice.deleteNeighborhoods(this.neighborhoodEdit.id).subscribe(
+            data => {
+              console.log(data)
+              if ( data.status == "deleted") {
+                swal({
+                  title: 'Registro eliminado con éxito',
+                  text: '',
+                  type: 'success',
+                  onClose: function reload() {
+                            location.reload();
+                          }
+                })
+              } 
+            },
+          error =>{
+            swal(
+              'No se pudo eliminar el registro',
+              '',
+              'warning'
+            )
+          })
+        } 
+      }
+    })
+  }
+
+  openModal (neighborhood) {
+    for (let i = 0; i < this.zones.length; i++) {
+      if ( neighborhood.zona == this.zones[i]['nombre']) {
+        this.zoneEdit = this.zones[i]['nombre'];
+        console.log(this.zoneEdit)
+      }
+    }
+    this.neighborhoodEdit = neighborhood;
     jQuery('#modal-see').modal('open');
     //document.getElementsByClassName('table-radio');
   }
@@ -80,11 +197,15 @@ export class NeighborhoodsComponent implements OnInit {
   }
 
   edit () {
-    jQuery('#codigoEdit').prop('disabled',false);
     jQuery('#nombreEdit').prop('disabled',false);
     jQuery('#selectEdit').prop('disabled',false);
     jQuery('#codigoEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
     jQuery('#nombreEdit').attr({style:' margin: 2px 0 7px 0 !important;'});
+    jQuery('#selectEdit').children('option[value="nodisplay"]').css('display','none');
+    jQuery('#selectEdit').on('change', () => {
+      this.neighborhood = jQuery('#selectEdit').val();
+      console.log(this.neighborhood)
+    });
   }
 
 }
