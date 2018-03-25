d17539ca28f3efeae66fe9165f2c5ff8b0fd9843
Melina <melinamejia95@gmail.com>
2018-03-25 15:41:44 -0500

    Automatic bills interface done

--- src/app/automaticbills/automaticbills.component.ts
+++ src/app/automaticbills/automaticbills.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let jQuery:any;
+
 @Component({
   selector: 'app-automaticbills',
   templateUrl: './automaticbills.component.html',
@@ -10,6 +12,18 @@ export class AutomaticbillsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    jQuery('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      today: 'Hoy',
+      clear: 'Limpiar',
+      close: 'Ok',
+      closeOnSelect: false, // Close upon selecting a date,
+      monthsFull: [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ],
+      weekdaysFull: [ 'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado' ],
+      weekdaysShort: [ 'Dom', 'Lun', 'Mar', 'Mier', 'Jue', 'Vier', 'Sáb' ],
+      format: 'yyyy-mm-dd'
+    });
   }
 
 }
